9af49c0ef3b66802d6b23dd18ba887170bfbf07d
Louis <tagnacht2@gmail.com>
2026-01-08 23:01:57 +0100

    downgrade swift even further

--- Package.swift
+++ Package.swift
@@ -1,11 +1,12 @@
-// swift-tools-version: 5.9
+// swift-tools-version: 5.6
 import PackageDescription
 
 let package = Package(
   name: "Pitchy",
+  
   products: [
 	.library(name: "Pitchy", targets: ["Pitchy"])
   ],
   targets: [
-	.target(name: "Pitchy", dependencies: [])
+	.target(name: "Pitchy", dependencies: [], path: "Source")
   ])
